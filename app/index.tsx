9f9c4eb04b104ffcf22f7b0a707adf33beae201f
markeuzebio <markeuzebio@gmail.com>
2025-02-14 18:40:46 -0300

    Implementa tela de candidatos a vaga
    A tela de candidatos foi implementada. Os
    dados "mocados" foram pegados emprestados
    do domínio https://randomuser.me/. A
    estilização por enquanto está genérica e
    sem muita glamour. Como o objetivo é a
    funcionalidade, optou-se por uma estilização
    um pouco rudimentar.
    
    Sistema de diretórios também foi alterado,
    Removendo o que não é (por enquanto) necessário
    e realocando alguns arquivos para diretórios
    que fazem sentido neste ponto do projeto.

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import CandidatesScreen from "./CandidatesScreen";
+
+export default function Index() {
+  return (
+    <View>
+      <CandidatesScreen/>
+    </View>
+  );
+}
