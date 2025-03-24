f620668c9f703a670aca33456415f6f16a02e7cd
markeuzebio <markeuzebio@gmail.com>
2025-03-24 17:27:40 -0300

    Mudar o nome dos campos para português

--- src/app/(auth)/register.tsx
+++ src/app/(auth)/register.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import CustomInput from "@/src/components/CostumInput";
 import CustomButton from '@/src/components/CustomButton';
 import { COLORS } from "@/src/constants/colors";
+import { useRouter } from 'expo-router';
 
 const Label = ({name}: {name: string}) => {
     return <Text style={styles.label}>{name}</Text>
@@ -14,6 +15,7 @@ const SignUpCandidate = () => {
     const [password, setPassword] = useState<string>('');
     const [location, setLocation] = useState<string>('');
     const [hourlyPay, setHourlyPay] = useState<string>('');
+    const router = useRouter();
     // const [foodOffer, setFoodOffer] = useState<boolean>(true);
     // const [desiredService, setDesiredService] = useState<string>('');
 
@@ -21,8 +23,8 @@ const SignUpCandidate = () => {
         console.warn("Sign Up Pressed");
     }
 
-    const onSignInPressed = () => {
-      console.warn("Sign In Pressed");
+    const goToSignInPage = () => {
+      router.dismissTo("/");
     }
     
     return (
@@ -30,7 +32,7 @@ const SignUpCandidate = () => {
         <ScrollView contentContainerStyle={styles.root} showsHorizontalScrollIndicator={false}>
           <Text style={styles.title}>Create an account</Text>
           <View style={styles.formItem}>
-              <Label name="Username" />
+              <Label name="Usuário" />
               <CustomInput value={username} onChange={setUsername} placeHolder="John Hooker" />
           </View>
           <View style={styles.formItem}>
@@ -38,19 +40,19 @@ const SignUpCandidate = () => {
               <CustomInput value={email} onChange={setEmail} placeHolder="johnny1934@example.com" />
           </View>
           <View style={styles.formItem}>
-              <Label name="Password" />
+              <Label name="Senha" />
               <CustomInput value={password} onChange={setPassword} placeHolder="Password" secureTextEntry />
           </View>
           <View style={styles.formItem}>
-              <Label name="Location" />
+              <Label name="Localização" />
               <CustomInput value={location} onChange={setLocation} placeHolder="1600 Amphitheatre Parkway, Mountain View, CA 94043, USA" secureTextEntry />
           </View>
           <View style={styles.formItem}>
-              <Label name="R$ / Hour" />
+              <Label name="R$ / Hora" />
               <CustomInput value={hourlyPay} onChange={setHourlyPay} placeHolder="12,00" secureTextEntry />
           </View>
-          <CustomButton title="Sign Up" onPress={onSignUpPressed} />
-          <CustomButton title="Already have an account? Sign In" onPress={onSignInPressed} type="TERTIARY" style={{marginTop: "auto"}}/>
+          <CustomButton title="Cadastrar" onPress={onSignUpPressed} />
+          <CustomButton title="Já tem uma conta? Entre" onPress={goToSignInPage} type="TERTIARY" style={{marginTop: "auto"}}/>
         </ScrollView>
       </SafeAreaView>
     );
